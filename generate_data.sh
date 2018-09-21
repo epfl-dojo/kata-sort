c14416651b6fbc8bfacb7ffa3aa2482d362a0ed3
Samuel Bancal <Samuel.Bancal@epfl.ch>
2018-09-21 16:41:43 +0200

    Les tableaux à trier sont stockés dans un JSON
    ... Pour importer le même lot depuis chaque language de façon
    simplifiée.

--- generate_data.sh
+++ generate_data.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 function data_random() {
-  echo -e [$(seq  $1 -1 1 | shuf | sed -z 's/\n/, /g'  | sed 's/, $//')]
+  echo $(seq $1 -1 1 | shuf | jq -R . | jq -s . )
 }
 function data_nearly_sorted() {
-  echo -e [$(for i in `seq 1 10 $1`; do seq $i 1 $((i+9)) | shuf;  done | sed -z 's/\n/, /g' | sed 's/, $//')]
+  echo $(for i in `seq 1 10 $1`; do seq $i 1 $((i+9)) | shuf;  done | jq -R . | jq -s . )
 }
 function data_reversed() {
-  echo -e [$(seq -s ', ' $1 -1 1)]
+  echo $(seq $1 -1 1 | jq -R . | jq -s . )
 }
 function data_few_unique() {
-  echo -e [$(for i in `seq $(($1/100)) -1 1`; do seq 199 -1 100 | shuf;  done | sed -z 's/\n/, /g' | sed 's/, $//')]
+  echo $(for i in `seq $(($1/100)) -1 1`; do seq 199 -1 100 | shuf;  done | jq -R . | jq -s . )
 }
 
 # Check that arg $1 is set and is a multiple of 100
@@ -23,15 +23,18 @@ else
   echo "     ... calling ./generate_data.sh $1"
   echo
   echo -e "\n# Shuffled numbers from 1 to $1"
-  echo -n "\$data_random = " 
-  data_random $1
+  DATA_RANDOM=$(data_random $1)
+  echo -e "data_random : ${DATA_RANDOM}\n"
   echo -e "\n# Roughly sorted numbers from 1 to $1"
-  echo -n "\$data_nearly_sorted = "
-  data_nearly_sorted $1
+  DATA_NEARLY_SORTED=$(data_nearly_sorted $1)
+  echo -e "data_nearly_sorted : ${DATA_NEARLY_SORTED}\n"
   echo -e "\n# Reversed numbers from $1 to 1"
-  echo -n "\$data_reversed = "
-  data_reversed $1
+  DATA_REVERSED=$(data_reversed $1)
+  echo -e "data_reversed : ${DATA_REVERSED}\n"
   echo -e "\n# $(($1/100)) times shuffled numbers from 199 to 100"
-  echo -n "\$data_few_unique = "
-  data_few_unique $1
+  DATA_FEW_UNIQUE=$(data_few_unique $1)
+  echo -e "data_few_unique : ${DATA_FEW_UNIQUE}\n"
+
+  echo -e "{\n  \"data_random\": ${DATA_RANDOM},\n  \"data_nearly_sorted\": ${DATA_NEARLY_SORTED},\n  \"data_reversed\": ${DATA_REVERSED},\n  \"data_few_unique\": ${DATA_FEW_UNIQUE}\n}" > generated_data.json
+  echo "Saved to generated_data.json"
 fi
